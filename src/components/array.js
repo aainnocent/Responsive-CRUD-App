eaee0800f99e6200aec8a6f67f876fa85e1af3c5
aainnocent <innocentamoaku95@gmail.com>
2024-08-13 23:45:21 +0200

    first commit

---
+++ src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+    {
+        id: "1",
+        Name: "Amoaku",
+        Age: "23",
+    },
+    {
+        id: "2",
+        Name: "Innocent",
+        Age: "22",
+    },
+    {
+        id: "3",
+        Name: "Elf",
+        Age: "23",
+    },
+];
+
+export default array;
